e73bf780286fbf6b94213f4d19267afc85b91ab8
sakuraba1005 <nodako5001koroken@yahoo.co.jp>
2019-04-26 14:14:20 +0900

    Hello World!

---
+++ app.js
@@ -0,0 +1,8 @@
+var http = require('http');//モジュール読み出し
+var server = http.createServer(function(request, response){
+
+  response.writeHead(200,{'Content-Type':'text/plain'});
+  response.write('Hello World\n');
+  response.end();
+});
+serever.listen(process.env.PORT || 8080);
