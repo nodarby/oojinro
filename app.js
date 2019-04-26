9c70b11b73db917ad9735c58718f42a44ca21ccd
nodarby <ndtmhr@outlook.jp>
2019-04-26 14:51:15 +0900

    わーい

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ var server = http.createServer(function(request, response){
   response.write('Hello World\n');
   response.end();
 });
-serever.listen(process.env.PORT || 8080);
+server.listen(process.env.PORT || 8080);
