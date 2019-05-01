713f4021c173727b70630400ede081fdc352ea9d
Saji Ayahito <saji.ayahito@h.mbox.nagoya-u.ac.jp>
2019-05-01 21:42:18 +0900

    Change 名前の変更の反映の実装

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -11,11 +11,22 @@
       userName: {
         get: function () { return this.$store.getters['user/name'] },
         set: function (value) { this.$store.commit('user/name', value) }
+      },
+      uuid: function () {
+        return this.$store.getters['name/uuid']
+      },
+      roomName: function () {
+        return this.$store.getters['room/name']
       }
     },
     methods: {
       done: function () {
         if (this.$router.history.current.query.redirect_to !== undefined){
+          // ルームに所属しているならば，ルームの情報も更新
+          if (this.roomName !== ''){
+            const socket = this.$store.getters['socket/socket']
+            socket.emit('requestUpdateUser', { uuid: this.uuid, roomName: this.roomName, userName: this.userName })
+          }
           this.$router.push({path: this.$router.history.current.query.redirect_to})
         }else{
           this.$router.push({path: '/'})
