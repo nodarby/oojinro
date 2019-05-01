832b802b796a92e2ddede79d5f675fa5eb38f217
nodarby <ndtmhr@outlook.jp>
2019-05-01 11:38:58 +0900

    Change app.jsでdist参照

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var http = require('http');//モジュール読み出し
-var server = http.createServer(function(request, response){
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+const PORT = process.env.PORT || 8080;
 
-  response.writeHead(200,{'Content-Type':'text/plain'});
-  response.write('Hello World\n');
-  response.end();
+app.use(express.static('frontend/dist'));
+
+http.listen(PORT, function(){
+    console.log('server listening. Port:' + PORT);
 });
-server.listen(process.env.PORT || 8080);
