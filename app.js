4cbf6d2660ba70794a0c9409be8cc49166770f1a
nodarby <ndtmhr@outlook.jp>
2019-05-01 11:56:23 +0900

    Add WebSocketの接続

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+const io = require('socket.io')(http);
 const PORT = process.env.PORT || 8080;
 
 app.use(express.static('frontend/dist'));
+
+io.on('connection',function(socket){
+  console.log('connected');
+//  socket.on('message',function(msg){
+//    console.log('message:'+msg);
+//  });
+});
 
 http.listen(PORT, function(){
     console.log('server listening. Port:' + PORT);
